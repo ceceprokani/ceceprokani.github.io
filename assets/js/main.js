dd9fae82e1f2e6b800f3d2d75ab63a5d63b44679
Cecep Rokani <cecepfahriazal1997@gmail.com>
2024-11-02 23:26:49 +0700

    fix: header toggle button

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,19 +13,11 @@
    * Header toggle
    */
   const headerToggleBtn = document.querySelector('.header-toggle');
-  const headerToggleBtnIcon = document.querySelector('.header-toggle-icon');
-  const headerToggleBtnText = document.querySelector('.header-toggle-text');
 
   function headerToggle() {
     document.querySelector('#header').classList.toggle('header-show');
-    headerToggleBtnIcon.classList.toggle('bi-list');
-    headerToggleBtnIcon.classList.toggle('bi-x');
-
-    if (headerToggleBtnIcon.classList.contains('bi-x')) {
-      headerToggleBtnText.innerHTML = 'Close'
-    } else {
-      headerToggleBtnText.innerHTML = 'Menu'
-    }
+    headerToggleBtn.classList.toggle('bi-list');
+    headerToggleBtn.classList.toggle('bi-x');
   }
   headerToggleBtn.addEventListener('click', headerToggle);
 
